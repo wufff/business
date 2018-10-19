c3137112c8dcc4ef3abb152cd35ce0f6b9d30ed3
wufff <wfqjnzb@sina.com>
2018-10-19 16:01:06 +0800

    test

--- static/js/initFont.js
+++
@@ -1,8 +0,0 @@
-setRem();
-window.addEventListener("orientationchange", setRem);
-window.addEventListener("resize", setRem); 
-function setRem() {
-	var html = document.querySelector("html");
-	var width = html.getBoundingClientRect().width;
-	html.style.fontSize = width / 16 + "px";adfa 
-}
